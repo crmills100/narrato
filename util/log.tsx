f8a48af2c7bb7fce189c9a396700d217d2c8915d
Christopher Mills <crmills100@yahoo.com>
2025-09-06 00:59:57 -0400

    new log functions

--- util/log.tsx
+++ util/log.tsx
@@ -5,6 +5,20 @@ export function log(msg: String) {
   _log += msg + "\n";
 }
 
+export function error(msg: String, error: any) {
+  console.error(msg, error);
+  _log += "ERROR: " + msg + " " + error + "\n";
+}
+
+export function warn(msg: String) {
+  console.error(msg);
+  _log += "WARN: " + msg + "\n";
+}
+
 export function getLog() {
   return _log;
+}    
+
+export function clearLog() {
+  return _log = "";
 }    
