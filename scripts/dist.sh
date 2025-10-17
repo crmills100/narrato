1126249370f60bded6dcd031d78431745c0d41ba
Christopher Mills <crmills100@yahoo.com>
2025-10-17 09:24:17 -0400

    reorg of dirs

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -4,29 +4,29 @@ mkdir www
 mkdir www/narrato
 mkdir www/narrato/images
 
-cp ./assets/story_list.json ./www/narrato
+cp ./stories/story_list.json ./www/narrato
 
-cd ./assets/basic_story
+cd ./stories/basic_story
 zip -r basic_story story.json thumbnail.jpg images audio
 cd ../../
-cp ./assets/basic_story/basic_story.zip ./www/narrato
+cp ./stories/basic_story/basic_story.zip ./www/narrato
 
-cd ./assets/music_demo
+cd ./stories/music_demo
 zip -r music_demo story.json thumbnail.jpg images audio
 cd ../../
-cp ./assets/music_demo/music_demo.zip ./www/narrato
+cp ./stories/music_demo/music_demo.zip ./www/narrato
 
-cd ./assets/lullaby_star
+cd ./stories/lullaby_star
 zip -r lullaby_star story.json thumbnail.jpg images audio
 cd ../../
-cp ./assets/lullaby_star/lullaby_star.zip ./www/narrato
-cp ./assets/lullaby_star/thumbnail.jpg ./www/narrato/images/ls_thumbnail.jpg
+cp ./stories/lullaby_star/lullaby_star.zip ./www/narrato
+cp ./stories/lullaby_star/thumbnail.jpg ./www/narrato/images/ls_thumbnail.jpg
 
-cd ./assets/totes_vol1_ep1
+cd ./stories/totes_vol1_ep1
 zip -r totes_vol1_ep1 story.json thumbnail.jpg images audio
 cd ../../
-cp ./assets/totes_vol1_ep1/totes_vol1_ep1.zip ./www/narrato
-cp ./assets/totes_vol1_ep1/thumbnail.jpg ./www/narrato/images/tm_thumbnail.png
+cp ./stories/totes_vol1_ep1/totes_vol1_ep1.zip ./www/narrato
+cp ./stories/totes_vol1_ep1/thumbnail.jpg ./www/narrato/images/tm_thumbnail.png
 
 cd ./www
 zip -r narrato_www.zip narrato
